2e4c5549b3945a31a4de2808dac4d33d1e868be7
Django Cass <dj.cass44@gmail.com>
2018-09-02 09:23:46 +1000

    Added initial demo setup

--- app/src/main/java/com/django/colorpicker/MainActivity.java
+++ app/src/main/java/com/django/colorpicker/MainActivity.java
@@ -2,12 +2,22 @@ package com.django.colorpicker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View viewCanvas = findViewById(R.id.viewCanvas);
+        Button buttonMain = findViewById(R.id.buttonMain);
+        buttonMain.setOnClickListener(view -> {
+
+        });
     }
 }
